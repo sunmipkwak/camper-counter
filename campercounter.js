68db791db1a7d81b309857d42383d5aac1fbc41a
sunmipkwak <127140480+sunmipkwak@users.noreply.github.com>
2023-03-07 03:37:39 +0900

    Add files via upload

---
+++ campercounter.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function checkin() {
+    count += 1
+    countEl.textContent = count
+}
+function save() { 
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = 0
+}
+
+
+
+
+
